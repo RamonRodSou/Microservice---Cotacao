fb4b5110a455954482504d16ee5ed6a4348f5063
Ramon <ramonrodsouza@gmail.com>
2025-11-01 22:06:37 -0300

    feature-004 - feat: Criando CurrencyPriceClient para buscar uma API Externa de moedas globais

---
+++ src/main/java/br/com/technosou/client/CurrencyPriceClient.java
@@ -0,0 +1,19 @@
+package br.com.technosou.client;
+
+import br.com.technosou.dto.CurrencyPriceDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(baseUri="https://economia.awesomeapi.com.br")
+@Path("/last")
+//@RegisterRestClient
+@ApplicationScoped
+public interface CurrencyPriceClient {
+
+    @GET
+    @Path("/{pair}")
+    CurrencyPriceDTO getPriceByPair(@PathParam("pair") String pair);
+}
